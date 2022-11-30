647a6686ea4c5e7383e40600c1a4d156b446c906
saurav4986 <saurav.raj4986@gmail.com>
2022-11-30 18:59:08 +0530

    Added: codes to validate Email of User

--- src/main/java/com/bridgeLabz/UserRegistrationForm.java
+++ src/main/java/com/bridgeLabz/UserRegistrationForm.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
- * (UC-2_Last_Name)
- As a User need to enter a valid Last Name
- - Last name starts with Cap and has minimum 3 characters
+ * (UC-3_Email)
+ As a User need to enter a valid email
+ - E.g. abc.xyz@bl.co.in
+ - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
  */
 public class UserRegistrationForm
 {
@@ -31,6 +32,16 @@ public class UserRegistrationForm
             System.out.println(" You have entered valid Last Name");
         } else {
             System.out.println("Sorry! you have entered invalid Last Name");
+        }
+
+        System.out.println("Please enter your Email Address: ");
+        String email = sc.next();
+        String emailPattern="^[a-z0-9]{3,}[+.-]?[a-z0-9]{0,}[@]{1,}[a-z0-9]{1,}[.]{1,}([a-z]{0,}[.]{0,})[a-z]{2,}$";
+        boolean emailCheck = Pattern.matches(emailPattern, email);
+        if (emailCheck) {
+            System.out.println("You have entered valid Email Address");
+        } else {
+            System.out.println("Sorry! you have entered invalid Email Address");
         }
     }
 }
