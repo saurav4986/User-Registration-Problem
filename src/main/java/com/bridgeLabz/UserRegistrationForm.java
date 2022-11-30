bacc8be84356e0ae1601914d2adf3f12569ddd30
saurav4986 <saurav.raj4986@gmail.com>
2022-11-30 18:41:12 +0530

    Added: codes to validate Last Name of User

--- src/main/java/com/bridgeLabz/UserRegistrationForm.java
+++ src/main/java/com/bridgeLabz/UserRegistrationForm.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
- * (UC-1_First_Name)
- As a User need to enter a valid First Name
- - First name starts with Cap and has minimum 3 characters
+ * (UC-2_Last_Name)
+ As a User need to enter a valid Last Name
+ - Last name starts with Cap and has minimum 3 characters
  */
 public class UserRegistrationForm
 {
@@ -22,6 +22,15 @@ public class UserRegistrationForm
             System.out.println("You have entered valid First Name");
         }else {
             System.out.println("Sorry! you have entered invalid First Name");
+        }
+
+        System.out.println("Please enter your Last Name: ");
+        String lastName = sc.next();
+        boolean res1 = Pattern.matches("^[A-Z][A-Za-z]{2,}$", lastName);
+        if (res1) {
+            System.out.println(" You have entered valid Last Name");
+        } else {
+            System.out.println("Sorry! you have entered invalid Last Name");
         }
     }
 }
