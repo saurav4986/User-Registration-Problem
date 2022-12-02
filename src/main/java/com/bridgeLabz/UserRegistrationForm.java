6f5e4c0c504957ab70a1a9624db5417b224c00a5
saurav4986 <saurav.raj4986@gmail.com>
2022-12-02 13:54:31 +0530

    Added: codes to validate Password(1-uppercase) of User

--- src/main/java/com/bridgeLabz/UserRegistrationForm.java
+++ src/main/java/com/bridgeLabz/UserRegistrationForm.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
- * (UC-5_Password)
+ * (UC-6_Pwd_UpperCase)
  As a User need to follow pre-defined Password rules.
  Rule1
  – minimum 8 Characters
+ - NOTE – All rules must be passed
+ Rule2
+ – Should have at least 1 Upper Case
  - NOTE – All rules must be passed
  */
 public class UserRegistrationForm
@@ -55,12 +58,16 @@ public class UserRegistrationForm
             System.out.println("Sorry! you have entered invalid Mobile Number");
         }
 
+        //Password:
         System.out.println("Please enter your Password: ");
-        String password = sc.next();
-        String passwordPattern="[A-Za-z0-9]{8,}$";
+        String password = sc.nextLine();
+        //UC5: Password with the rule of minimum 8 characters
+        //String passwordPattern="[A-Za-z0-9]{8,}$"; should have minimum 8 characters
+        //UC6: Password with the rule of minimum 1 capital letter
+        String passwordPattern="^(?=.*[A-Z])([A-Za-z0-9]){8,}$";
         boolean passwordCheck = Pattern.matches(passwordPattern,password);
         if (passwordCheck) {
-            System.out.println("You have entered valid Password");
+            System.out.println("Fantastic! You have entered valid Password");
         } else {
             System.out.println("Sorry! you have entered invalid Password");
         }
